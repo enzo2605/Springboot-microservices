2e02831a8c203c0179ad63f2ac92e445117bb75b
Vincenzo Iannucci <34918941+enzo2605@users.noreply.github.com>
2023-08-09 14:43:59 +0200

    Recuperato tutto

---
+++ restful-web-services/src/main/java/com/vincenzo/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,43 @@
+package com.vincenzo.rest.webservices.restfulwebservices.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserResource {
+    private UserDaoService service;
+
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    // GET /users
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable("id") int id) {
+        User user = service.findOne(id);
+        if (user == null) {
+            throw new UserNotFoundException("id: " + id);
+        }
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User returnedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(returnedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+}
