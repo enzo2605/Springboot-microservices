362882fadd4b63272a3b6c34309a7a797febf111
Vincenzo Iannucci <34918941+enzo2605@users.noreply.github.com>
2023-08-10 16:08:34 +0200

    Request Header

--- restful-web-services/src/main/java/com/vincenzo/rest/webservices/restfulwebservices/versoning/VersoningPersonController.java
+++ restful-web-services/src/main/java/com/vincenzo/rest/webservices/restfulwebservices/versoning/VersoningPersonController.java
@@ -24,4 +24,14 @@ public class VersoningPersonController {
     public PersonV2 getSecondVersionOfPersonRequestParameter() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonRequestHeader() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
 }
