d30ecb7493b5def9e385226256cdbf5f42bcbb94
Vincenzo Iannucci <34918941+enzo2605@users.noreply.github.com>
2023-08-11 16:17:48 +0200

    Spring JPA

--- learn-jpa-and-hibernate/src/main/java/com/vincenzo/springbot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/vincenzo/springbot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -1,21 +1,24 @@
-package com.vincenzo.springbot.learnjpaandhibernate.course.jdbc;
+package com.vincenzo.springbot.learnjpaandhibernate.course;
 
-import com.vincenzo.springbot.learnjpaandhibernate.course.Course;
+import com.vincenzo.springbot.learnjpaandhibernate.course.jdbc.CourseJdbcRepository;
+import com.vincenzo.springbot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+public class CourseCommandLineRunner implements CommandLineRunner {
+    //@Autowired
+    //private CourseJdbcRepository repository;
     @Autowired
-    private CourseJdbcRepository repository;
+    private CourseJpaRepository repository;
     @Override
     public void run(String... args) throws Exception {
         repository.insert(new Course(1, "Learn AWS Now!", "Mammt"));
         repository.insert(new Course(2, "Learn Azure Now!", "Mammt"));
         repository.insert(new Course(3, "Learn DevOps Now!", "Mammt"));
 
-        repository.delete(1);
+        repository.deleteById(1);
 
         System.out.println(repository.findById(2));
         System.out.println(repository.findById(3));
---
+++ learn-jpa-and-hibernate/src/main/java/com/vincenzo/springbot/learnjpaandhibernate/course/jpa/CourseJpaRepository.java
@@ -0,0 +1,27 @@
+package com.vincenzo.springbot.learnjpaandhibernate.course.jpa;
+
+import com.vincenzo.springbot.learnjpaandhibernate.course.Course;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public class CourseJpaRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void insert(Course course) {
+        entityManager.merge(course);
+    }
+
+    public Course findById(long id) {
+        return entityManager.find(Course.class, id);
+    }
+
+    public void deleteById(long id) {
+        Course find = entityManager.find(Course.class, id);
+        entityManager.remove(find);
+    }
+}
