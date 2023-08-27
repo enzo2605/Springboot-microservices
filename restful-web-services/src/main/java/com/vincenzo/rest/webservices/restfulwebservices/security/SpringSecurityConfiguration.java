492ddef30586abe54f7a649d9cbb0f32541651df
Vincenzo Iannucci <34918941+enzo2605@users.noreply.github.com>
2023-08-27 10:55:14 +0200

    Create SpringSecurityConfiguration.java

---
+++ restful-web-services/src/main/java/com/vincenzo/rest/webservices/restfulwebservices/security/SpringSecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.vincenzo.rest.webservices.restfulwebservices.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SpringSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        // 1) All request should be authenticated
+        httpSecurity.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+
+        // 2) If a request is not authenticated, a web page is shown
+        httpSecurity.httpBasic(withDefaults());
+
+        // 3) Disable CSRF, in order to call PUT and POST requests
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
+    }
+}
