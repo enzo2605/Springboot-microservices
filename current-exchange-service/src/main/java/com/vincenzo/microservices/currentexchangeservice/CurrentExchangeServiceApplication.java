3d9aef341e49e76f71f05fad6b1b30576f2f735d
Vincenzo Iannucci <34918941+enzo2605@users.noreply.github.com>
2023-09-03 16:50:05 +0200

    Currency exchange service

---
+++ current-exchange-service/src/main/java/com/vincenzo/microservices/currentexchangeservice/CurrentExchangeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vincenzo.microservices.currentexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrentExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrentExchangeServiceApplication.class, args);
+	}
+
+}
