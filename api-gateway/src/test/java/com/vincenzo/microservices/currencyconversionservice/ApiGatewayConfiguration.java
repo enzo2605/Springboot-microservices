04d082c06d82587535c05bf7f466d204d7f49b99
Vincenzo Iannucci <34918941+enzo2605@users.noreply.github.com>
2023-09-24 17:05:52 +0200

    Updated

---
+++ api-gateway/src/test/java/com/vincenzo/microservices/currencyconversionservice/ApiGatewayConfiguration.java
@@ -0,0 +1,20 @@
+package com.vincenzo.microservices.currencyconversionservice;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/dio")
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
+
+}
